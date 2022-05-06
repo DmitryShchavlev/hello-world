69f84450fa10af72e7216470bb2b0fd8ff11a6c1
Dmitry <itachi-kisame@mail.ru>
2022-05-06 21:49:10 +0500

    created NewCompNewBranch

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div>
+  <NewCompNewBranch/>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NewCompNewBranch from '@/components/NewCompNewBranch'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    NewCompNewBranch
   }
 }
 </script>
